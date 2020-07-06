e4c8bed19fd42714f6cf56f9d4c66eb29eea6d8d
Ramil <ram.ru@mail.ru>
2020-07-06 11:23:16 +0300

    Homework5

--- src/geekbrains/lesson/Main.java
+++ src/geekbrains/lesson/Main.java
@@ -16,7 +16,7 @@ public class Main {
         float[] arr = new float[size];
         Arrays.fill(arr, 1);
 
-        for(int i=0;i<arr.length;i++){
+        for(int i=0;i<arr.length; i++){
             arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
 
         }
